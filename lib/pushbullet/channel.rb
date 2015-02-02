16b3184a5ba24a12394a752133dcb64821d46f72
diogorosa <diogo.m.c.rosa@gmail.com>
2015-02-02 00:08:21 +0000

    Add channel missing methods
    subscribe, unsubscribe and get-info
    
    Signed-off-by: diogorosa <diogo.m.c.rosa@gmail.com>

--- lib/pushbullet/channel.rb
+++ lib/pushbullet/channel.rb
@@ -1,5 +1,23 @@
 module Pushbullet
   class Channel < Resource
     include Pushable
+
+    def self.subscribe(name)
+      create(channel_tag: name)
+    end
+
+    def self.unsubscribe(idn)
+      Pushbullet.client.delete "#{path}/#{idn}"
+      true
+    end
+
+    def self.get_info(tag)
+      Pushbullet.client.get("channel-info?tag=#{tag}")
+      true
+    end
+
+    def self.path
+      'subscriptions'
+    end
   end
 end
